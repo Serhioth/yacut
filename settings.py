9c95510199d44ffde01a19d5bd6cbeb95b57e102
serhioth <senchos.ork@gmail.com>
2024-01-18 06:29:14 +0300

    DZ20 Fix Settings

--- settings.py
+++ settings.py
@@ -2,7 +2,9 @@ import os
 
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI',
+                                        default='sqlite:///db.sqlite3')
     JSON_AS_ASCII = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.getenv('SECRET_KEY')
+    SECRET_KEY = os.getenv('SECRET_KEY',
+                           default='CHEAT_SECRET')
